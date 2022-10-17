fc5be45f61f4b339d61e84021d60fd0afd102261
acf200 <thorben.schomacker@haw-hamburg.de>
2022-10-17 18:08:44 +0000

    masking split added

--- create_dataset.py
+++ create_dataset.py
@@ -3,16 +3,15 @@ import zipfile
 from tqdm.auto import tqdm
 import spacy
 import argparse
-from random import shuffle
+from random import shuffle, randint
 
-def file_to_sentence_list(nlp,zip_file_object,dataset_file,shuffle_sentences=True):
-    print('start reading the file')
-    sentences =[]
+def file_to_sentence_list(nlp,zip_file_object,mask_percentage,shuffle_sentences=True):
+    print('start reading the zip-file')
+    sentence_tuples =[]
     for file_name in tqdm(zip_file_object.namelist()):
         # only consider text-files
         if '.txt' in file_name:
             with zip_file_object.open(file_name) as current_file:
-                f = open(dataset_file, "a")
                 for line_bytes in current_file.readlines():
                     line_str = line_bytes.decode()
                     # skip small paragraphs such as headlines etc.
@@ -21,13 +20,68 @@ def file_to_sentence_list(nlp,zip_file_object,dataset_file,shuffle_sentences=Tru
                         #sentences = [i for i in nlp(text).sents]
                         for sentence in nlp(text).sents:
                             sentence_clean = str(sentence).replace('\n','').replace('«','"').replace('»','"')
-                            sentences.append(sentence_clean)
-    print('Successfully read',len(sentences),'sentences from', len(zip_file_object.namelist()), 'files')
+                            sentence_tuples.append( (mask_sentence(sentence_clean, mask_percentage) ,
+                                               sentence_clean) )
+    print('Successfully read',len(sentence_tuples),'sentences from', len(zip_file_object.namelist()), 'files')
     if shuffle_sentences:
-        shuffle(sentences)
-    with open(dataset_file, "w") as f:
-        for sentence in sentences:
-            f.write(sentence+'\n')
+        shuffle(sentence_tuples)
+    return sentence_tuples
+
+        
+def mask_sentence(sentence:str, mask_percentage):
+    masked_words = []
+    for word in sentence.split(' '):
+        if randint(0, 100)  > mask_percentage:
+            masked_words.append(word)
+        else:
+            # masking
+            masked_words.append('<mask>')
+    return ' '.join(masked_words)
+
+
+def write_sentences(data_dir, sentence_tuples, tags, args):
+    train_source_path = data_dir+'/train_source.txt'
+    train_target_path = data_dir+'/train_target.txt'
+
+    val_source_path = data_dir+'/val_source.txt'
+    val_target_path = data_dir+'/val_target.txt'
+
+    test_source_path = data_dir+'/test_source.txt'
+    test_target_path = data_dir+'/test_target.txt'
+
+    # clear all files
+    for file_name in [train_source_path, train_target_path, 
+                      val_source_path, val_target_path, 
+                      test_source_path, test_target_path]:
+        with open(file_name, "w") as f:
+            f.write('')
+        f.close()
+        
+    
+    validate_index = int(len(sentence_tuples)/args.validate_percentage)
+    test_index = validate_index+int(len(sentence_tuples)/args.test_percentage)
+
+    for masked_sentence, sentence in sentence_tuples[:validate_index]:
+        with open(val_source_path, "a") as f:
+            f.write(tags[0]+masked_sentence+'\n')
+        f.close()
+        with open(val_target_path, "a") as f:
+            f.write(tags[1]+sentence+'\n')
+        f.close()
+    for masked_sentence, sentence in sentence_tuples[validate_index:test_index]:
+        with open(test_source_path, "a") as f:
+            f.write(tags[0]+masked_sentence+'\n')
+        f.close()
+        with open(test_target_path, "a") as f:
+            f.write(tags[1]+sentence+'\n')
+        f.close()
+    
+    for masked_sentence, sentence in sentence_tuples[test_index:]:
+        with open(train_source_path, "a") as f:
+            f.write(tags[0]+masked_sentence+'\n')
+        f.close()
+        with open(train_target_path, "a") as f:
+            f.write(tags[1]+sentence+'\n')
         f.close()
 
 
@@ -40,9 +94,9 @@ def main():
         help='The name or path of the spacy model.'
     )
     parser.add_argument(
-        '--output_file',
+        '--data_dir',
         type=str,
-        default='output/domain_adaptation_sentences.txt',
+        default='output',
         help='Name or directory of the output file.'
     )
     parser.add_argument(
@@ -57,29 +111,61 @@ def main():
         default=True,
         help='Whether or not to shuffle the sentences before writing them.'
     )
+    parser.add_argument(
+        '--add_tags',
+        nargs='+',
+        default=['de_DE', 'de_DE'],
+        help='First tag is the of the source and the second is of the target data. Leave blank if you dont want to use tags.'
+    )
+    parser.add_argument(
+        '--mask_percentage',
+        type=bool,
+        default=15,
+        help='Percentage of words, that are going to be masked.'
+    )
+    parser.add_argument(
+        '--validate_percentage',
+        type=bool,
+        default=10,
+        help='Percentage of sentence, that will be in the validation dataset.'
+    )
+    parser.add_argument(
+        '--test_percentage',
+        type=bool,
+        default=10,
+        help='Percentage of sentence, that will be in the test dataset.'
+    )
     
     args = parser.parse_args()
     
     spacy_model_name = args.spacy_model
+    tags = args.add_tags
+    if len(tags)==0:
+        tags = ['','']
+    elif len(tags)==2:
+        tags = [tags[0]+' ',tags[0]+' ']
+    else:
+        raise ValueError('Number of tags should be 0 or 2')
     
     
     # setup spacy
     spacy.cli.download(spacy_model_name)
     nlp = spacy.load(spacy_model_name)
+    url = args.input_url
     
     # Some other options for the url to consider:
     ## url = 'https://textgridlab.org/1.0/aggregator/zip/query?query=*&filter=edition.agent.value%3AKafka%2C+Franz&filter=format%3Atext%2Fxml&filter=work.genre%3Aprose&transform=text&meta=false&only=text/xml&dirnames='
     ## url = 'https://textgridlab.org/1.0/aggregator/zip/query?query=*&filter=format%3Atext%2Fxml&filter=work.genre%3Aprose&transform=text&meta=false&only=text/xml&dirnames='
     
-    url = args.input_url
+    
     print('Retrieve the zip-file from\n', url)
     filehandle, _ = urllib.request.urlretrieve(url)
     zip_file_object = zipfile.ZipFile(filehandle, 'r')
+    sentence_tuples = file_to_sentence_list(nlp,zip_file_object,args.mask_percentage, args.shuffle)
+    
+    write_sentences(args.data_dir, sentence_tuples, tags, args)
+    
 
-    
-    dataset_file = args.output_file
-    
-    file_to_sentence_list(nlp,zip_file_object,dataset_file, args.shuffle)
             
     
 if __name__ == "__main__":
